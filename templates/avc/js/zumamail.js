4fda19edff7cbd9ddeee4c7ce78a5038cb07be03
4xtrot <4xtro2@gmail.com>
2013-10-13 22:10:54 +0400

    Добавлены полоски сверху снизу. Добавлены sliders (фото под меню) на все страницы Отюстированы header, footer, menu, slider Кардинально переделан index.php Поправил вывод результатов поиска Убрал лишний код в index.php Добавил вывод фото в результаты поиска Добавил модуль zumamail.js по шифрованию email

---
+++ templates/avc/js/zumamail.js
@@ -0,0 +1,26 @@
+function zumaMail(){
+
+
+var z_tmp = '<a href="mailto:vkstan@mail.ru" mce_href="mailto:vkstan@mail.ru">отправить нам быстрое сообщение.</a>'
+//TODO добавить шифрование после согласования метода и почтового адреса
+
+var z1 ='sup';
+var z2 ='ef="m';
+var z3 ='ailt';
+var z4 ='ocod';
+var z5 ='upport@';
+var z8 ='geoco';
+var z7 ='din';
+var z6 ='g.2b';
+var z9 ='g.ru">';
+var z10 ='<a hr';
+var z11 ='port@ge';
+var z12 ='o: s';
+var z13 ='ing.2';
+var z14 ='bg.r';
+var z15 ='u<a/>';
+
+document.write(z_tmp /*z10+z2+z3+z12+z5+z8+z7+z6+z9+z1+z11+z4+z13+z14+z15*/);
+}
+
+zumaMail();
